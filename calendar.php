ab312bc11ed4c3a47e4fd1a7e1afc864c86b63e3
Emelie Tham <Emelie Tham>
2017-04-20 09:19:06 +0200

    Adding widget files

---
+++ calendar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Calendar</title>
+<link href="menubar.css" rel="stylesheet">
+<link href="calendar.css" rel="stylesheet">
+<script type="text/javascript" src="calendar_load_week.js" defer></script>
+</head>
+
+<body>
+  <ul>
+    <li><a href="homepage.php">HOME </a></li>
+    <li><a class="active" href="calendar.php">CALENDAR</a></li>
+    <li><a href="personal_routines.php">PERSONAL ROUTINES</a></li>
+    <li><a href="import_export.php">IMPORT &amp; EXPORT</a></li>
+    <li><a href="settings.php">SETTINGS</a></li>
+    <li style="float:right"><a href="">LOGOUT</a></li>
+  </ul>
+
+  <div id="weekHead"> "MIA" </div>
+
+  <div class="week">
+    <div id="monbox"> Monday </div>
+    <div id="tuebox"> Tuesday </div>
+    <div id="wedbox"> Wednesday </div>
+    <div id="thubox"> Thursday </div>
+    <div id="fribox"> Friday </div>
+    <div id="satbox"> Saturday </div>
+    <div id="sunbox"> Sunday </div>
+  </div>
+
+</body>
+</html>
