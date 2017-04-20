56189ae27186c0f2b36ec02fa9149e7b6fcddeca
Msoderman <msoderm@kth.se>
2017-04-20 08:58:29 +0200

    Made importCal into a function, returns json

--- importCal.php
+++ importCal.php
@@ -18,93 +18,99 @@ class event {
 	public $DESCRIPTION = NULL;
 	public $LOCATION = NULL;
 }
-
-
-$file_content = file_get_contents("https://www.kth.se/social/user/218278/icalendar/9fd7a5485dbf6243bbd0b4ada27934ab4f028163");
-$file = explode("BEGIN:VEVENT", $file_content);
-for ($i = 1; $i < count($file); $i++) {
-	$file[$i] = "BEGIN:VEVENT:" . $file[$i];
-	$attr = explode("\r\n", $file[$i]);
-	$events[] = new event;
-	$sum = "";
-	foreach ($attr as $a) {
-		if ($sum !== "") {
-			if (ltrim($a) !== $a) {
-				$a = ltrim($a);
-				$sum = $sum . $a;
-			}
-			else {
-				$events[$i-1]->SUMMARY = $sum;
-				break;
-			}
-		}
-		if (strstr($a, "SUMMARY:") && $sum == "") {
-			$sum = strstr_after($a, "SUMMARY:");
-		}
-	}
-	
-	foreach ($attr as $a) {
-		if (strstr($a, "DTSTART;VALUE=DATE-TIME:")) {
-			$sum = strstr_after($a, "DTSTART;VALUE=DATE-TIME:");
-			$events[$i-1]->DTSTART = $sum;
-		} else if (strstr($a, "DTSTART:")) {
-			$sum = strstr_after($a, "DTSTART:");
-			$events[$i-1]->DTSTART = $sum;
-		}
-	}
-	
-	foreach ($attr as $a) {
-		if (strstr($a, "DTEND;VALUE=DATE-TIME:")) {
-			$sum = strstr_after($a, "DTEND;VALUE=DATE-TIME:");
-			$events[$i-1]->DTEND = $sum;
-		} else if (strstr($a, "DTEND:")) {
-			$sum = strstr_after($a, "DTEND:");
-			$events[$i-1]->DTSTART = $sum;
-		}
-	}
-	
-	foreach ($attr as $a) {
-		if (strstr($a, "UID:")) {
-			$sum = strstr_after($a, "UID:");
-			$events[$i-1]->UID = $sum;
-		}
-	}
-	
-	$sum = "";
-	foreach ($attr as $a) {
-		if ($sum !== "") {
-			if(ltrim($a) !== $a) {
-				$a = ltrim($a);
-				$sum = $sum . $a;
-			}
-			else {
-				$events[$i-1]->DESCRIPTION = $sum;
-				break;
-			}
-		}
-		if (strstr($a, "DESCRIPTION:") && $sum == "") {
-			$sum = strstr_after($a, "DESCRIPTION:");
-		}
-	}
-	
-	$sum = "";
-	foreach ($attr as $a) {
-		if ($sum !== "") {
-			if(ltrim($a) !== $a) {
-				$a = ltrim($a);
-				$sum = $sum . $a;
-			}
-			else {
-				$events[$i-1]->LOCATION = $sum;
-				break;
-			}
-		}
-		if (strstr($a, "LOCATION:") && $sum == "") {
-			$sum = strstr_after($a, "LOCATION:");
-		}
-	}
+function downloadFile ($fileLink) {
+	$file_content = file_get_contents($fileLink);
+	$e = importCal($file_content);
+	return $e;
 }
 
-$e = json_encode($events);
-echo $e;
+function importCal ($file_content) {
+	$file = explode("BEGIN:VEVENT", $file_content);
+	for ($i = 1; $i < count($file); $i++) {
+		$file[$i] = "BEGIN:VEVENT:" . $file[$i];
+		$attr = explode("\r\n", $file[$i]);
+		$events[] = new event;
+		$sum = "";
+		foreach ($attr as $a) {
+			if ($sum !== "") {
+				if (ltrim($a) !== $a) {
+					$a = ltrim($a);
+					$sum = $sum . $a;
+				}
+				else {
+					$events[$i-1]->SUMMARY = $sum;
+					break;
+				}
+			}
+			if (strstr($a, "SUMMARY:") && $sum == "") {
+				$sum = strstr_after($a, "SUMMARY:");
+			}
+		}
+		
+		foreach ($attr as $a) {
+			if (strstr($a, "DTSTART;VALUE=DATE-TIME:")) {
+				$sum = strstr_after($a, "DTSTART;VALUE=DATE-TIME:");
+				$events[$i-1]->DTSTART = $sum;
+			} else if (strstr($a, "DTSTART:")) {
+				$sum = strstr_after($a, "DTSTART:");
+				$events[$i-1]->DTSTART = $sum;
+			}
+		}
+		
+		foreach ($attr as $a) {
+			if (strstr($a, "DTEND;VALUE=DATE-TIME:")) {
+				$sum = strstr_after($a, "DTEND;VALUE=DATE-TIME:");
+				$events[$i-1]->DTEND = $sum;
+			} else if (strstr($a, "DTEND:")) {
+				$sum = strstr_after($a, "DTEND:");
+				$events[$i-1]->DTSTART = $sum;
+			}
+		}
+		
+		foreach ($attr as $a) {
+			if (strstr($a, "UID:")) {
+				$sum = strstr_after($a, "UID:");
+				$events[$i-1]->UID = $sum;
+			}
+		}
+		
+		$sum = "";
+		foreach ($attr as $a) {
+			if ($sum !== "") {
+				if(ltrim($a) !== $a) {
+					$a = ltrim($a);
+					$sum = $sum . $a;
+				}
+				else {
+					$events[$i-1]->DESCRIPTION = $sum;
+					break;
+				}
+			}
+			if (strstr($a, "DESCRIPTION:") && $sum == "") {
+				$sum = strstr_after($a, "DESCRIPTION:");
+			}
+		}
+		
+		$sum = "";
+		foreach ($attr as $a) {
+			if ($sum !== "") {
+				if(ltrim($a) !== $a) {
+					$a = ltrim($a);
+					$sum = $sum . $a;
+				}
+				else {
+					$events[$i-1]->LOCATION = $sum;
+					break;
+				}
+			}
+			if (strstr($a, "LOCATION:") && $sum == "") {
+				$sum = strstr_after($a, "LOCATION:");
+			}
+		}
+	}
+
+	$e = json_encode($events);
+	echo $e;
+	return $e;
+}
 ?>
