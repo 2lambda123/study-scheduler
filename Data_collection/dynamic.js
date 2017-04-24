b5fd7bfa4ff08b96e70c75b6f121cba837069e9b
Harald Ng <hng@kth.se>
2017-04-24 10:32:38 +0200

    Removed lab changes

--- Data_collection/dynamic.js
+++ Data_collection/dynamic.js
@@ -1,5 +1,4 @@
 var number = 1;
-var laboration = false;
 
 function addNewField() { //adds a new coursework
     var container = document.getElementById("container"); //parent container
@@ -25,45 +24,37 @@ function addNewField() { //adds a new coursework
     hp.name = "hp_work" + number;
 
     //13 appendChild (used in removeField as static variable)
-    if (laboration != true) {
-        container.appendChild(document.createTextNode("Laboration"));
-        container.appendChild(lab);
-        container.appendChild(document.createElement("br"));
-        laboration = true;
-        }
 
-        container.appendChild(document.createTextNode("Coursework " + number + ": ")); //Adds text
-        container.appendChild(coursework); //Adds element created above
-        container.appendChild(document.createElement("br"));
+    container.appendChild(document.createTextNode("Coursework " + number + ": ")); //Adds text
+    container.appendChild(coursework); //Adds element created above
+    container.appendChild(document.createElement("br"));
 
-        container.appendChild(document.createTextNode("Startdate: "));
-        container.appendChild(startdate);
-        container.appendChild(document.createElement("br"));
+    container.appendChild(document.createTextNode("Startdate: "));
+    container.appendChild(startdate);
+    container.appendChild(document.createElement("br"));
 
-        container.appendChild(document.createTextNode("Enddate: "));
-        container.appendChild(enddate);
-        container.appendChild(document.createElement("br"));
+    container.appendChild(document.createTextNode("Enddate: "));
+    container.appendChild(enddate);
+    container.appendChild(document.createElement("br"));
 
-        container.appendChild(document.createTextNode("HP: "));
-        container.appendChild(hp);
-        container.appendChild(document.createElement("br"));
-        container.appendChild(document.createElement("br"));
+    container.appendChild(document.createTextNode("HP: "));
+    container.appendChild(hp);
+    container.appendChild(document.createElement("br"));
+    container.appendChild(document.createElement("br"));
 
-        number++; //increment to keep track of number of courseworks
-    }
+    number++; //increment to keep track of number of courseworks
+}
 
-    function removeField() { //removes a coursework
+function removeField() { //removes a coursework
 
-      if (number > 1){ //prevent button to be removed
+    if (number > 1) { //prevent button to be removed
         number--;
 
         var loop = 13; // if lab checkbox exists, then must loop 16 times
-        if (number == 1)
-            loop = 16;
 
         for (var i = 0; i < loop; i++) //Loop through all container.appendChild done in addNewField
             container.removeChild(container.lastChild); //Removes last child from container
-      }
-
-
     }
+
+
+}
