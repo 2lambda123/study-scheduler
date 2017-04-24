f325139f5a31e3f3a3325efd169e92ed8131a6f0
Msoderman <msoderm@kth.se>
2017-04-24 16:50:32 +0200

    Fixed removing labs, taking events from test.txt for now

--- labsChosen.php
+++ labsChosen.php
@@ -1,5 +1,18 @@
 <?php
 $user = "labs";
-file_put_contents($user.='.txt',json_encode($_POST));
+$e = json_decode(file_get_contents("test.txt"));
+$count = count($e);
+
+for ($i = 0; $i < $count; $i++) {
+	if (!in_array($e[$i]->DTSTART, $_POST)) {
+		echo $e[$i]->SUMMARY;
+		unset($e[$i]);
+		$e = array_values($e);
+		$i--;
+		$count = count($e);
+	}
+}
+
+file_put_contents($user.='.txt',json_encode($e));
 
 ?>
