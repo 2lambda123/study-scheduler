cb206f2b1feb75650b0b1039e19e31b69f1dbe2d
Msoderman <msoderm@kth.se>
2017-04-27 16:54:14 +0200

    Tried fixing DB.php

--- DB.php
+++ DB.php
@@ -1,13 +1,24 @@
 <?php
+class db {
+	private $mysqli;
 
-
-
-
-
-
-
-
-
-
-
+	function connect() {
+		$this->mysqli = new mysqli('localhost', 'root', '', "studyscheduler");
+		
+		if ($this->mysqli->connect_errno) {
+			echo "<br><h1>Failed to connect to MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error . "</h1><br>";
+		}
+	}
+	
+	function runQ($sql) {
+		$stmt = $this->mysqli->prepare($sql);
+		$stmt->execute();	
+	}
+	
+	function getQ($sql) {
+		$stmt = $this->mysqli->prepare($sql);
+		$stmt->execute();
+		return $stmt;
+	}
+}
 ?>
