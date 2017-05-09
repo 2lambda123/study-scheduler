677a1b8d26433dfcc9609f24355590b26c444f62
Philip Wester <Philip Wester>
2017-05-09 11:00:28 +0200

    added export button and made exported calendars unique for users

--- site/calExpImp.php
+++ site/calExpImp.php
@@ -1,9 +1,31 @@
+<!-- PAGE FOR DOWNLOADING/EXPORTING THE CALENDAR ics FILE -->
+<!-- NEEDS: SESSION["uuid"] -->
+<!-- UPDATES/imports from DB: With every refresh of the page -->
+
 <!DOCTYPE html>
 <html>
 <title>Import &amp; Export</title>
 <link href="menubar.css" rel="stylesheet">
-
 <body>
-  <?php include 'menubar.php'; ?>
+  <?php
+    session_start();
+    //TESTDATA
+    $_SESSION["uuid"] = 'c7fe7b83-2be5-11e7-b210-f0795931a7ef';
+    // END OF TESTDATA
+    include 'menubar.php';
+    include '..\scripts\DB.php';
+    include '..\scripts\importCal.php';
+    include '..\algorithm\export.php';
+    $sessID = $_SESSION["uuid"];
+    $db = new DB();
+    // Imports the calendar associated with the uuid from the DB
+    $cal = $db -> select("SELECT STUDY FROM calendar WHERE ID = '$sessID'");
+  	$cal = $cal[0]["STUDY"];
+    // Stores the imported calendar in a calendar file
+    export($cal, $sessID);
+    // The route to the updated/new calendar file
+    $calRoute = "..\site\calendar_" . $sessID . ".ics";
+  ?>
   <h1>Import &amp; Export</h1>
+  <a href= "<?php echo $calRoute ?>" download>EXPORT/DOWNLOAD CALENDAR</a>
 </body>
