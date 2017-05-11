3e2bd16936cb8494aee4c34d02a71233fcc7ebbf
Harald Ng <hng@kth.se>
2017-05-11 16:44:51 +0200

    Added favicon and logo

--- site/calExpImp.php
+++ site/calExpImp.php
@@ -4,8 +4,12 @@
 
 <!DOCTYPE html>
 <html>
+<head>
+  <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
 <title>Import &amp; Export</title>
 <link href="menubar.css" rel="stylesheet">
+</head>
 <body>
   <?php
     session_start();
