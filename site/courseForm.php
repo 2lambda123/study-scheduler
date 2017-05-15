3d15794c0dcfad971289f9cda59cc6812d3177e8
Harald Ng <hng@kth.se>
2017-05-15 09:35:27 +0200

    Fixed dynamic coursework and minor dynamic changes

--- site/courseForm.php
+++ site/courseForm.php
@@ -1,9 +1,8 @@
 <script src="../ajax/dynamic.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="../ajax/ajax.js"></script>
 	<form class="form" action='../ajax/receivePersonal.php' method='post'>
 		<h3>New course</h3>
-		<div id="container">
+		<div id="a">
 			<input type="submit"/><br/><br/>
 			Course code: <input type="text" name="coursecode"/><br/>
 			Exam: <input type="checkbox" name="exam"> HP: <input name="hp_exam" type="float"/><br/>
@@ -11,7 +10,7 @@
 			Exam date: <input name="courseend" type="date"/><br/><br/>
 			Laboration: <input type="checkbox" name="lab"> HP: <input name="hp_lab" type="float"/><br/>
 			Number of labs: <input name="numberoflabs" type="text"/><br/><br/>
-			
+
 			<a href="#" id="add" onclick="addNewField()"> [Add] </a>
 			<a href="#" id="remove" onclick="removeField()">[Remove] </a><br/>
 		</div>
