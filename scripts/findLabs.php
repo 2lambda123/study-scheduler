3bb02d717285b92141fdda9be79965e0bb95bf6b
PhilipW <PhilipW@example.com>
2017-05-16 16:02:54 +0200

    stoped pupuplabs when no labs are found

--- scripts/findLabs.php
+++ scripts/findLabs.php
@@ -2,11 +2,18 @@
 //Old name: LabFind.php
 //Function labFind recieves a json_encoded event array. It returns an json
 //encoded array with all the labs from the recieved calendar.
+include_once '../algorithm/find.php';
 function labFind($file){
   $e = json_decode($file);
   $eLabs = array();
+
+  $now = time();
   // Finds all events with the SUMMARY that contasins the word "Laboration".
   for($i = 0; $i < count($e); $i++){
+    //A guard to exclude all events that happen before $now
+		if(strtotime(substr(eventTimeFloat($e[$i] -> DTSTART), 0, 8) . substr(eventTimeFloat($e[$i] -> DTSTART), 10, 6)) <= eventTimeFloat($now)) {
+			continue;
+    }
     if(strpos($e[$i] -> SUMMARY, 'Laboration') !== false){ //Case sensetive
       array_push($eLabs, $e[$i]);
     }
